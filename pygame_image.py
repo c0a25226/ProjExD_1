4f84b224ae9dd8d1153e4c30f9bf646afd398956
HosodaRei <c0a25226d3@edu.teu.ac.jp>
2026-04-14 16:27:27 +0900

    練習5~10

--- pygame_image.py
+++ pygame_image.py
@@ -1,30 +1,39 @@
 import os
 import sys
 import pygame as pg
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg") #練習１:背景画像
+    bg_img = pg.image.load("fig/pg_bg.jpg")  # 練習1
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 練習8
+    kk_img = pg.image.load("fig/3.png")  # 練習3
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  # 練習10：こうかとんRectの取得
+    kk_rct.center = 300, 200  # 練習10：こうかとんの初期座標の設定
     tmr = 0
-    koukaton = pg.image.load("fig/3.png") #練習3:コウカトン読み込み
-    koukaton = pg.transform.flip(koukaton, True ,False)#練習3:コウカトンの反転(関数,左右,上下)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [0, 0]) #練習2
-        screen.blit(koukaton,[300,200]) #練習４：コウカトンの初期位置
+        key_lst = pg.key.get_pressed()  # 練習10：キー取得
+        if key_lst[pg.K_UP]:  # 上
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  # 下
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:  # 左
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  # 右
+            kk_rct.move_ip(+1, 0)
+        x = tmr%3200  # 練習5
+        screen.blit(bg_img, [-x, 0])  # 練習2
+        screen.blit(bg_img2, [-x+1600, 0])  # 練習7
+        screen.blit(bg_img, [-x+3200, 0])  # 練習9
+        screen.blit(kk_img, kk_rct)  # 練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
-
-
+        clock.tick(200)  # 練習6
 if __name__ == "__main__":
     pg.init()
     main()
     pg.quit()
-    sys.exit()
